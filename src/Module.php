aab00e57ca2c1615678f45c0c6a873839096dbe1
Dániel Buga <bugadani@gmail.com>
2014-02-12 22:01:19 +0100

    Update to use new configuration methods.

--- src/Module.php
+++ src/Module.php
@@ -17,57 +17,87 @@ class Module extends \Miny\Modules\Module
     public function defaultConfiguration()
     {
         return array(
-            'cache' => array(
-                'http_cache'    => array(
-                    'enabled'        => false,
-                    'cache_lifetime' => 600,
-                    'paths'          => array()
-                ),
-                'apc'           => array(
-                    'storage_key' => 'miny_cache'
-                ),
-                'session'       => array(
-                    'storage_key' => 'miny_cache'
-                ),
-                'orm'           => array(
-                    'table' => 'miny_cache'
-                ),
-                'sqlite_memory' => array(
-                    'table' => 'miny_cache'
-                ),
-                'sql'           => array(
-                    'table' => 'miny_cache'
-                ),
-                'default_cache' => __NAMESPACE__ . '\\Drivers\\Session',
-            )
+            'http_cache'    => array(
+                'enabled'        => false,
+                'cache_lifetime' => 600,
+                'paths'          => array()
+            ),
+            'apc'           => array(
+                'storage_key' => 'miny_cache'
+            ),
+            'session'       => array(
+                'storage_key' => 'miny_cache'
+            ),
+            'orm'           => array(
+                'table' => 'miny_cache'
+            ),
+            'sqlite_memory' => array(
+                'table' => 'miny_cache'
+            ),
+            'sql'           => array(
+                'table' => 'miny_cache'
+            ),
+            'default_cache' => __NAMESPACE__ . '\\Drivers\\Session'
         );
     }
 
     public function init(BaseApplication $app)
     {
-        $factory    = $app->getContainer();
-        $parameters = $app->getParameterContainer();
+        $container = $app->getContainer();
 
-        $factory->addAlias(__NAMESPACE__ . '\Drivers\SQL', null, array(1 => '@cache:sql:table'));
-        $factory->addAlias(__NAMESPACE__ . '\Drivers\Session', null, array('@cache:session:storage_key'));
-        $factory->addAlias(__NAMESPACE__ . '\Drivers\APC', null, array('@cache:apc:storage_key'));
-        $factory->addAlias(__NAMESPACE__ . '\Drivers\ORM', null, array(1 => '@cache:orm:table'));
-        $factory->addAlias(__NAMESPACE__ . '\Drivers\SQLite_Memory', null, array('@cache:sqlite_memory:table'));
+        $container->addConstructorArguments(
+            __NAMESPACE__ . '\\Drivers\\SQL',
+            array(
+                1 => array($this->getConfiguration('sql:table'))
+            )
+        );
+        $container->addConstructorArguments(
+            __NAMESPACE__ . '\\Drivers\\Session',
+            array($this->getConfiguration('session:storage_key'))
+        );
+        $container->addConstructorArguments(
+            __NAMESPACE__ . '\\Drivers\\APC',
+            array($this->getConfiguration('apc:storage_key'))
+        );
+        $container->addConstructorArguments(
+            __NAMESPACE__ . '\\Drivers\\ORM',
+            array(
+                1 => array(
+                    $this->getConfiguration(
+                        'orm:table'
+                    )
+                )
+            )
+        );
+        $container->addConstructorArguments(
+            __NAMESPACE__ . '\\Drivers\\SQLite_Memory',
+            array($this->getConfiguration('sqlite_memory:table'))
+        );
 
-        $factory->addAlias(__NAMESPACE__ . '\AbstractCacheDriver', $parameters['cache']['default_cache']);
+        $container->addAlias(
+            __NAMESPACE__ . '\\AbstractCacheDriver',
+            array($this->getConfiguration('default_cache'))
+        );
 
-        if ($parameters['cache']['http_cache']['enabled']) {
-            $factory->addAlias('\Miny\HTTP\Response', __NAMESPACE__ . '\ResponseCache\CachedResponse');
-
-            $httpCache = $factory->get(
-                __NAMESPACE__ . '\ResponseCache\HTTPCache',
-                array(3 => '@cache:http_cache:cache_lifetime')
-            );
-            $httpCache->addPaths($parameters['cache']['http_cache']['paths']);
-
-            $events = $factory->get('\Miny\Event\EventDispatcher');
-            $events->register('filter_request', array($httpCache, 'fetch'));
-            $events->register('filter_response', array($httpCache, 'store'));
+        if (!$this->getConfiguration('http_cache:enabled')) {
+            return;
         }
+
+        $container->addAlias(
+            '\\Miny\\HTTP\\Response',
+            __NAMESPACE__ . '\\ResponseCache\\CachedResponse'
+        );
+
+        $httpCache = $container->get(
+            __NAMESPACE__ . '\\ResponseCache\\HTTPCache',
+            array(
+                3 => array($this->getConfiguration('http_cache:cache_lifetime'))
+            )
+        );
+        $httpCache->addPaths(array($this->getConfiguration('http_cache:paths')));
+
+        $events = $container->get('\\Miny\\Event\\EventDispatcher');
+        $events->register('filter_request', array($httpCache, 'fetch'));
+        $events->register('filter_response', array($httpCache, 'store'));
     }
 }
